cd1f9d95a24bf92c7e97ae66a38f53b9be50086e
yzhh <2596322644@qq.com>
2022-03-11 22:03:46 +0800

    Update __init__.py

--- modules/status/__init__.py
+++ modules/status/__init__.py
@@ -8,13 +8,15 @@ from core.elements import FetchTarget, IntervalTrigger
 from core.bots.aiocqhttp.client import bot
 
 
+class Send:
+    send = False
+
+
 bot_id = 2314163511
-send_ = False
 
 
 @on_schedule('bot_status', IntervalTrigger(minutes=10))
 async def _(target: FetchTarget):
-    nonlocal send_
     command = '~echo ' + ''.join(random.sample(string.ascii_letters + string.digits, 8))
     send = await bot.send_private_msg(user_id=bot_id, message=command)
     flag = asyncio.Event()
@@ -22,10 +24,10 @@ async def _(target: FetchTarget):
     await asyncio.sleep(30)
     all_tsk = MessageTaskManager.get()
     if bot_id in all_tsk:
-        if not send_:
+        if not Send.send:
             await target.post_message('bot_status', '警告：小可服务状态异常，可能存在冻结或软件问题。')
-            send_ = True
+            Send.send = True
     else:
-        send_ = False
+        Send.send = False
 
 
